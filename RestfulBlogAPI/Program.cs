8fae1f415376028d1e4279af1dd71732703c0126
Peter Tabatt <ptabatt@gmail.com>
2023-12-08 16:09:41 -0700

    Fix Entity Framework versioning and configure swagger properly.

--- RestfulBlogAPI/Program.cs
+++ RestfulBlogAPI/Program.cs
@@ -1,65 +1,17 @@
 namespace RestfulBlogAPI
 {
-    //public class Program
-    //{
-    //    public static void Main(string[] args)
-    //    {
-    //        CreateHostBuilder(args).Build().Run();
-    //    }
-
-    //    public static IHostBuilder CreateHostBuilder(string[] args) =>
-    //        Host.CreateDefaultBuilder(args)
-    //            .ConfigureWebHostDefaults(webBuilder =>
-    //            {
-    //                webBuilder.UseStartup<Startup>();
-    //            });
-    //}
-
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseCors(builder =>
-                {
-                    builder.WithOrigins("https://your-production-frontend-app.com")
-                           .AllowAnyMethod()
-                           .AllowAnyHeader();
-                });
-
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- RestfulBlogAPI/Startup.cs
+++ RestfulBlogAPI/Startup.cs
@@ -1,64 +1,79 @@
-﻿//using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
-//public class Startup
-//{
-//    public IConfiguration Configuration { get; }
+public class Startup
+{
+    public IConfiguration Configuration { get; }
 
-//    public Startup(IConfiguration configuration)
-//    {
-//        Configuration = configuration;
-//    }
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
 
-//    public void ConfigureServices(IServiceCollection services)
-//    {
-//        // Configure DbContext
-//        services.AddDbContext<ApplicationDbContext>(options =>
-//            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Configure DbContext
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
-//        // Add services
-//        //services.AddScoped<IAuthService, AuthService>();
-//        //services.AddScoped<IPostService, PostService>(); 
+        services.AddDefaultIdentity<ApplicationUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            // Other configuration options as needed
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>();
 
-//        services.AddControllers();
-//    }
+        services.AddScoped<IAuthService, AuthService>();
 
-//    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-//    {
-//        if (env.IsDevelopment())
-//        {
-//            app.UseSwagger();
-//            app.UseSwaggerUI();
+        // Add services
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IPostService, PostService>();
 
-//            app.UseDeveloperExceptionPage();
-//        }
-//        else
-//        {
-//            app.UseCors(builder =>
-//            {
-//                builder.WithOrigins("https://your-production-frontend-app.com")
-//                       .AllowAnyMethod()
-//                       .AllowAnyHeader();
-//            }); 
-            
-//            app.UseExceptionHandler("/Home/Error");
-//            app.UseHsts();
-//        }
+        services.AddControllers();
 
-//        app.UseHttpsRedirection();
-        
-//        app.UseStaticFiles();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Blog API", Version = "v1" });
+        });
+    }
 
-//        app.UseAuthentication();
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
-//        app.UseRouting();
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseCors(builder =>
+            {
+                builder.WithOrigins("https://your-production-frontend-app.com")
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            });
 
-//        app.UseAuthorization();
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
 
-//        app.UseEndpoints(endpoints =>
-//        {
-//            endpoints.MapControllerRoute(
-//                name: "default",
-//                pattern: "{controller=Home}/{action=Index}/{id?}");
-//        });
-//    }
-//}
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        app.UseAuthentication();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
