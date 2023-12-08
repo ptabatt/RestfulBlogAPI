d48ce84a2d75596747cfbe5c38756648550b56d1
Peter Tabatt <ptabatt@gmail.com>
2023-12-07 19:52:38 -0700

    Reverting back to a working API for now.

--- RestfulBlogAPI/Startup.cs
+++ RestfulBlogAPI/Startup.cs
@@ -1,64 +1,64 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.EntityFrameworkCore;
 
-public class Startup
-{
-    public IConfiguration Configuration { get; }
+//public class Startup
+//{
+//    public IConfiguration Configuration { get; }
 
-    public Startup(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
+//    public Startup(IConfiguration configuration)
+//    {
+//        Configuration = configuration;
+//    }
 
-    public void ConfigureServices(IServiceCollection services)
-    {
-        // Configure DbContext
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+//    public void ConfigureServices(IServiceCollection services)
+//    {
+//        // Configure DbContext
+//        services.AddDbContext<ApplicationDbContext>(options =>
+//            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
-        // Add services
-        //services.AddScoped<IAuthService, AuthService>();
-        //services.AddScoped<IPostService, PostService>(); 
+//        // Add services
+//        //services.AddScoped<IAuthService, AuthService>();
+//        //services.AddScoped<IPostService, PostService>(); 
 
-        services.AddControllers();
-    }
+//        services.AddControllers();
+//    }
 
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+//    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//    {
+//        if (env.IsDevelopment())
+//        {
+//            app.UseSwagger();
+//            app.UseSwaggerUI();
 
-            app.UseDeveloperExceptionPage();
-        }
-        else
-        {
-            app.UseCors(builder =>
-            {
-                builder.WithOrigins("https://your-production-frontend-app.com")
-                       .AllowAnyMethod()
-                       .AllowAnyHeader();
-            }); 
+//            app.UseDeveloperExceptionPage();
+//        }
+//        else
+//        {
+//            app.UseCors(builder =>
+//            {
+//                builder.WithOrigins("https://your-production-frontend-app.com")
+//                       .AllowAnyMethod()
+//                       .AllowAnyHeader();
+//            }); 
             
-            app.UseExceptionHandler("/Home/Error");
-            app.UseHsts();
-        }
+//            app.UseExceptionHandler("/Home/Error");
+//            app.UseHsts();
+//        }
 
-        app.UseHttpsRedirection();
+//        app.UseHttpsRedirection();
         
-        app.UseStaticFiles();
+//        app.UseStaticFiles();
 
-        app.UseAuthentication();
+//        app.UseAuthentication();
 
-        app.UseRouting();
+//        app.UseRouting();
 
-        app.UseAuthorization();
+//        app.UseAuthorization();
 
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-        });
-    }
-}
+//        app.UseEndpoints(endpoints =>
+//        {
+//            endpoints.MapControllerRoute(
+//                name: "default",
+//                pattern: "{controller=Home}/{action=Index}/{id?}");
+//        });
+//    }
+//}
